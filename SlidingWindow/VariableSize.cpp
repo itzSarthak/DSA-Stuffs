a4a5ef81d4d02534ed320936e5ca99d338787693
Sarthak Rai <142003426+itzSarthak@users.noreply.github.com>
2023-09-21 23:16:05 +0530

    VariableSize.cpp
    Variable Size Template

---
+++ SlidingWindow/VariableSize.cpp
@@ -0,0 +1,22 @@
+## Template for Variable Size Slding window
+
+    int i=0,j=0;
+    type k;                 ** Here k deontes some condition . 
+/*
+    while(j < arr.size())
+    {
+        ** Calculations 
+        if(cond == k)
+            ** Determine the Answer from Calculations.
+
+        else
+        {
+            ** Basically here we are compressing the window.
+
+            while(cond > k)
+                ** Slide the Window.
+        }
+        j++;
+    }
+    return res;
+*/
